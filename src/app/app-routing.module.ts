d62dc2cae6c3e7a29b27b1f03b03ac06be751d16
salmakl <salma.kalkhi@gmail.com>
2022-06-10 15:34:14 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { LoginComponent } from './components/login/login.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FirstComponent } from './components/forms/first/first.component';
+import { ButtonComponent } from './components/button/button.component';
+
+const routes: Routes = [
+  { path: 'login',component: LoginComponent },
+  {path: 'home', component:LandingPageComponent },
+  {path: 'nav', component:HeaderComponent },
+  {path: 'footer', component:FooterComponent },
+  {path: 'form', component:FirstComponent },
+  {path: 'button', component:ButtonComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FirstComponent } from './components/forms/first/first.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    FirstComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
